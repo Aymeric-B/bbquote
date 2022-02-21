beb1d3493adfd3a2a46da612bed2b20f7f34a3c1
aymeric boffa <aymeric.boffa@gmail.com>
2022-02-21 16:06:07 +0100

    CI-CD

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 mkdir -p ~/.streamlit/
 
-# echo "[general]
-# email = \'${HEROKU_EMAIL_ADDRESS}'
-# " > ~/.streamlit/credentials.toml
+echo "[general]
+email = \'${HEROKU_EMAIL_ADDRESS}'
+" > ~/.streamlit/credentials.toml
 
 echo "[server]
 headless = true
